c577c375ec46f374dde5ca4ebc2e98dc3b9b8218
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-18 00:03:38 +0530

    you done it dude

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app.include_router(validation_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
